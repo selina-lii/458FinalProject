a126ca41579d57ef649692b20ece0e78d43f2733
Vincent Chen <vchen7142@gmail.com>
2026-04-01 20:54:21 -0400

    Update asyncTester.py
    Extremely slow first implementation of live signal plotting

--- asyncTester.py
+++ asyncTester.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-
+import random
 
 WINDOW = 2
 V_REF = 5.0
@@ -13,41 +13,18 @@ ADC_STEPS = (2**R_BITS) - 1
 buf = []
 
 y_range = [-5,5]
-x_len = 1000
+x_range = [0,100]
+x_len = 100
 
-fig = plt.figure(figsize=(14,7))
-ax = fig.add_subplot(1,1,1)
+fig = plt.figure(figsize=(7,3.5))
+ax = fig.add_subplot(111)
 ax.set_ylim(y_range)
+ax.set_xlim(x_range)
 plt.subplots_adjust(bottom=0.20)
 xs = [0]
-ys = [None]
+ys = [[0,0,0,0,0]]
 
-line = ax.plot(xs,ys,color='#33ebff')
-
-def animate(frame,xs,ys):
-    timestamp = xs[-1]+1
-    data_sample = buf[-1]
-    xs.append(timestamp)
-    xs = xs[-x_len:]
-    ax.set_xlim([xs[0],xs[-1]])
-    
-    ys.append(data_sample)
-    
-    ys = ys[-x_len:]
-    
-    ylimits = ax.get_ylim()
-    if(data_sample < ylimits[0]):
-        new_limit = abs(data_sample - 0.1)
-        ax.set_ylim(-new_limit, new_limit)
-    if(data_sample > ylimits[1]):
-        new_limit = data_sample + 0.1
-        ax.set_ylim(-new_limit, new_limit)
-        
-    ax.set_xticks(xs)
-    plt.xticks(rotation=45, ha='right')
-    
-    line.set_data(xs,ys)
-    
+line = ax.plot(xs,ys)
 
 def preprocess(samples):
     # First two values are EMG, last three values are EOG
@@ -64,24 +41,50 @@ def classify(features):
 # ── async pipeline ───────────────────────────────────────────────
 async def main():
     print("I'm in main")
-    ser = serial.Serial('COM7', 115200, timeout=0)
+    #ser = serial.Serial('COM7', 115200, timeout=0)
 
     async def read():
+        global xs,ys,fig
         while True:
-            if ser.in_waiting:
+            #if ser.in_waiting:
                 try:
-                    data = ser.readline()
-                    splitData = [data[i:i+2] for i in range(0,10,2)]
-                    intData = [0.0] * 5
-                    for i in range(5):
-                        # Turn ints into voltage values
-                        intData[i] = (int.from_bytes(splitData[i], "little")/ADC_STEPS) * V_REF
+                    #data = ser.readline()
+                    #splitData = [data[i:i+2] for i in range(0,10,2)]
+                    #intData = [0.0] * 5
+                    #for i in range(5):
+                    #    # Turn ints into voltage values
+                    #    intData[i] = (int.from_bytes(splitData[i], "little")/ADC_STEPS) * V_REF
+                    intData = [random.randrange(-5,5),
+                               random.randrange(-5,5),
+                               random.randrange(-5,5),
+                               random.randrange(-5,5),
+                               random.randrange(-5,5)]
                     buf.append(intData)
                     #print(intData)
                 except Exception as e:
                     print(e)
                     pass
-            await asyncio.sleep(0)
+                
+                # Plotting part
+                timestamp = xs[-1]+1
+                data_sample = intData
+                if len(xs) < x_len:
+                    xs.append(timestamp % x_len)
+                    xs = xs[-x_len:]
+    
+                ys.append(data_sample)
+    
+                ys = ys[-x_len:]
+    
+                #print(len(xs))
+                #print(xs)
+                plt.cla()
+                ax.plot(xs,ys)
+                ax.set_xlim(x_range)
+                plt.pause(0.01)
+                if not plt.fignum_exists(fig.number):
+                    break
+                await asyncio.sleep(0)
 
     async def process():
         timeToSleep = WINDOW
@@ -92,18 +95,10 @@ async def main():
                 snap, buf[:] = buf[:], []
                 print(f"n={len(snap)}, result={classify(preprocess(snap))}, time elapsed={time.time()-startTime}")
             timeToSleep = WINDOW - (time.time()-startTime)
+            
+    await asyncio.gather(read(), process())
 
-    async def plot():
-        try:
-            plt.show(block=False)
-            await asyncio.sleep(0)
-        except KeyboardInterrupt:
-            pass
-    await asyncio.gather(read(), process(), plot())
-
-anim = animation.FuncAnimation(fig=fig,
-                func=animate,
-                fargs=(xs,ys),
-                interval=1)
 print("Started Running")
+plt.ion()
+plt.show()
 asyncio.run(main())
